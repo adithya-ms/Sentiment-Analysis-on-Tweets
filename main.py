6b10d7924ae41f0e96e8e5c1b23d1290ea420520
adithya-ms <adithya.ms15@gmail.com>
2021-06-02 12:36:26 +0200

    Start of preprocessing

--- main.py
+++ main.py
@@ -2,22 +2,41 @@
 
 from data_loading import load_data
 from baseline_models import splitData, kNN, SVM
+from sklearn.pipeline import Pipeline
+import preprocessing
+import pdb
 
-data = load_data()
-#print(data)
+def main():
+	data = load_data()
+	#print(data)
 
-contents = data["content"]
-labels = data["sentiment"]
-print(labels)
+	contents = data["content"]
+	labels = data["sentiment"]
+	#print(labels)
 
-""" Preprocessing (not yet implemented) """ 
+	""" Train - val - test Split """
+	inputs_train, inputs_test, labels_train, labels_test = splitData(contents, labels)
+
+	""" Preprocessing (not yet implemented) """ 
+	pdb.set_trace()
+	inputs_train = preprocessing.remove_url(inputs_train)
+	inputs_train = preprocessing.remove_mentions(inputs_train)
+	inputs_train = preprocessing.remove_hashtags(inputs_train)
+
+	'''
+	pipe = Pipeline([('scaler', StandardScaler()),
+					 ('svc', SVC())])
+	'''
+	""" Baseline implementation"""
+
+	# Does not yet work because of missing preprocessing, so commented out
+	# predictionKNN = kNN(inputs_train, labels_train, inputs_test, 13)
+	# predictionSVM = SVM(inputs_train, labels_train, inputs_test, 'linear')
+
+	# Implement some accuracy calculation (predicted vs. true labels accuracy + analysis)
 
 
-""" Baseline implementation"""
-inputs_train, inputs_test, labels_train, labels_test = splitData(contents, labels)
 
-# Does not yet work because of missing preprocessing, so commented out
-# predictionKNN = kNN(inputs_train, labels_train, inputs_test, 13)
-# predictionSVM = SVM(inputs_train, labels_train, inputs_test, 'linear')
 
-# Implement some accuracy calculation (predicted vs. true labels accuracy + analysis)
+if __name__ == "__main__":
+	main()
