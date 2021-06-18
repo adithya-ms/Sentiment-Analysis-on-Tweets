9ee3fd08cd500cbccad47a7670b1ec60d0b90251
Erwinbruining <erwinbruining@gmail.com>
2021-06-18 17:07:21 +0200

    Final version BERT

--- bert_model.py
+++ bert_model.py
@@ -6,37 +6,29 @@ import numpy as np
 import tensorflow as tf
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
-from baseline_models import splitData
+from baseline_models import splitData, print_confusion_matrix
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from transformers import BertForSequenceClassification, BertTokenizer
 import preprocessing
+import csv
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
 parser = argparse.ArgumentParser(description="Sentiment analysis")
-parser.add_argument("--reload_model", type=str, help="Path of model to reload")
-parser.add_argument("--save_model", type=str, help="Path for saving the model")
 parser.add_argument("--batch_size", type=int, default=32)
 parser.add_argument("--epochs", type=int, default=1)
-# parser.add_argument("--num_hidden_layers", type=int, default=1)
-# parser.add_argument("--num_attn_heads", type=int, default=1)
 
 
 class Dataset(Dataset):
-    def __init__(self, data, labels, tokenizer):
+    def __init__(self, data, labels_encoded, tokenizer):
         super().__init__()
-
-        #encoding labels
-        le = LabelEncoder()
-        labels_encoded = le.fit_transform(labels)
-
         # Making lists for: labels, input_ids, attention_masks
         labels = []
         input_ids = []
         attention_masks = []
-        for line, label in zip(data,labels_encoded):
+        for line, label in zip(data, labels_encoded):
             encoded_dict = tokenizer.encode_plus(
                 line,  # Sentence to encode.
                 add_special_tokens=True,  # Add '[CLS]' and '[SEP]' tokens to sentences
@@ -54,32 +46,41 @@ class Dataset(Dataset):
         self.labels = torch.Tensor(labels)
 
     def __getitem__(self, index):
-        return self.input_ids[index].long().to(device),self.attention_masks[index].long().to(device), self.labels[index].long().to(device)
+        return self.input_ids[index].long().to(device), self.attention_masks[index].long().to(device), self.labels[
+            index].long().to(device)
 
     def __len__(self):
         return len(self.input_ids)
 
 
-
-def evaluate(model, loader):
+def evaluate(model, loader, le):
     model.eval()
     with torch.no_grad():
-        correct = 0.0 
+        correct = 0.0
         total = 0.0
+        y_true = []
+        y_pred = []
+
         for batch in loader:
             input_ids, attention_masks, labels = batch
-            out = model(input_ids=input_ids, token_type_ids=None, attention_mask= attention_masks,labels=labels,return_dict=True)
+            out = model(input_ids=input_ids, token_type_ids=None, attention_mask=attention_masks, labels=labels,
+                        return_dict=True)
             preds = out.logits.argmax(dim=-1)
+
+            output = out.logits.argmax(dim=-1).cpu().numpy()
+            y_pred.extend(output)
+            labels_ = labels.cpu().numpy()
+            y_true.extend(labels_)
 
             correct += (preds == labels).sum().item()
             total += labels.numel()
 
-
-    print("predicited ",correct, " out of ", total, " Correctly")
+    print("predicited ", correct, " out of ", total, " Correctly")
     model.train()
-    return correct/total
+    return correct / total, y_true, y_pred
 
-def train(model, train_loader, valid_loader, epochs):
+
+def train(model, train_loader, valid_loader, epochs, le):
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
     for epoch in range(epochs):
         start_time = time.time()
@@ -88,52 +89,42 @@ def train(model, train_loader, valid_loader, epochs):
         for i, batch in enumerate(train_loader):
 
             if i % 5 == 0 and i > 0:
-                tps = total_tokens/ (time.time()-start_time)
-                print("[Epoch %d, Iter %d] loss: %.4f, toks/sec: %d" % (epoch, i, total_loss/5, tps))
+                tps = total_tokens / (time.time() - start_time)
+                print("[Epoch %d, Iter %d] loss: %.4f, toks/sec: %d" % (epoch, i, total_loss / 5, tps))
                 start_time = time.time()
                 total_loss = 0.0
                 total_tokens = 0.0
 
             input_ids, attention_masks, labels = batch
             optimizer.zero_grad()
-            out = model(input_ids=input_ids,token_type_ids=None, attention_mask=attention_masks, labels=labels, return_dict=True)
+            out = model(input_ids=input_ids, token_type_ids=None, attention_mask=attention_masks, labels=labels,
+                        return_dict=True)
             out.loss.backward()
             optimizer.step()
             total_loss += out.loss.item()
             total_tokens += input_ids.numel()
 
-        acc = evaluate(model, valid_loader)
+        acc, y_true, y_pred = evaluate(model, valid_loader, le)
         print("[Epoch %d] Acc (valid): %.4f" % (epoch, acc))
-        start_time = time.time() # so tps stays consistent
+        start_time = time.time()
 
     print("############## END OF TRAINING ##############")
-    acc = evaluate(model, valid_loader)
+    acc, y_true, y_pred = evaluate(model, valid_loader, le)
+    print("Confusion matrix BERT:")
+    y_pred = le.inverse_transform(y_pred)
+    y_true = le.inverse_transform(y_true)
+    print_confusion_matrix(y_true, y_pred)
     print("Final Acc (valid): %.4f" % (acc))
 
 
-if __name__ == "__main__":
-    torch.manual_seed(0)
-    np.random.seed(0)
-    args = parser.parse_args()
-    args.save_model = "model.pt"
-    
-    pretrained = 'bert-base-cased'
-    tokenizer = BertTokenizer.from_pretrained(pretrained)
-    tokenizer.do_basic_tokenize = True
-
-    #########
-    """ Loads in the twitter data"""
-    df = pd.read_csv('data/text_emotion.csv')
-    df.head()
-    data = df["content"]
-    labels_raw = df["sentiment"]
+def label_merge(labels_raw):
     labels = []
     pos = []
     neg = []
     neutral = []
     for label in labels_raw:
         if label == "anger":
-           labels.append("neg")
+            labels.append("neg")
         elif label == "boredom":
             labels.append("neg")
         elif label == "hate":
@@ -157,7 +148,6 @@ if __name__ == "__main__":
         elif label == "surprise":
             labels.append("neutral")
         else:
-            print(label)
             labels.append("neutral")
     for label in labels:
         if label == "neutral":
@@ -167,46 +157,60 @@ if __name__ == "__main__":
         elif label == "neg":
             neg.append(label)
     print("label dist:")
-    print("pos= ",len(pos))
+    print("pos= ", len(pos))
     print("neg= ", len(neg))
     print("neutral= ", len(neutral))
+    return labels
 
-    """ Preprocessing (not yet implemented) """
 
-    """ Baseline implementation"""
-    inputs_train, inputs_test, labels_train, labels_test = splitData(data, labels)
-    #########
+def encoding_labels(labels, le):
+    labels_encoded = le.fit_transform(labels)
+    return labels_encoded
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    np.random.seed(0)
+    args = parser.parse_args()
+    args.save_model = "model.pt"
+
+    pretrained = 'bert-base-cased'
+    tokenizer = BertTokenizer.from_pretrained(pretrained)
+
+    """ Loads in the twitter data"""
+    df_train = pd.read_csv('data/train.csv')
+    df_train.head()
+    inputs_train = df_train["content"]
+    labels_train = df_train["sentiment"]
+    # For merged labels:
+    # labels_train = label_merge(labels_train)
+    le = LabelEncoder()
+    labels_train = encoding_labels(labels_train, le)
+
+    df_test = pd.read_csv('data/test.csv')
+    df_test.head()
+    inputs_test = df_test["content"]
+    labels_test = df_test["sentiment"]
+    # For merged labels:
+    # labels_test = label_merge(labels_test)
+    labels_test = encoding_labels(labels_test, le)
 
     train_dataset = Dataset(inputs_train, labels_train, tokenizer)
 
     train_loader = DataLoader(train_dataset,
-        shuffle=True,
-        batch_size=args.batch_size)
+                              shuffle=True,
+                              batch_size=args.batch_size)
     valid_dataset = Dataset(inputs_test, labels_test, tokenizer)
     valid_loader = DataLoader(valid_dataset,
-        batch_size=args.batch_size)
+                              batch_size=args.batch_size)
 
-    # # test_dataset = POSDataset("data/test.en", "data/test.en.label", tokenizer)
-    # test_dataset = POSDataset("data/test.en", None, tokenizer)
-    # test_loader = DataLoader(test_dataset,
-    #     batch_size=args.batch_size)
-    #
-
-    # Load a pretrained model
+    # Load a pretrained BERTbase - uncased model
     model = BertForSequenceClassification.from_pretrained(
-        pretrained, 
+        pretrained,
         num_labels=3,
         # num_hidden_layers=args.num_hidden_layers,
         # num_attention_heads=args.num_attn_heads,
         output_attentions=True)
     model = model.to(device)
 
-    # Load model weights from a file
-    # args.reload_model = "model.pt"
-    # if args.reload_model:
-    #     model.load_state_dict(torch.load(args.reload_model))
-
-    train(model, train_loader, valid_loader, args.epochs)
-
-    if args.save_model:
-        torch.save(model.state_dict(), args.save_model)
+    train(model, train_loader, valid_loader, args.epochs, le)
